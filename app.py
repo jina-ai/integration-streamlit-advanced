a82cb4dc28ab9dd5e5aba928ba85d50b51ae3330
Alex Cureton-Griffiths <alexcg@outlook.com>
2020-12-04 11:05:07 +0100

    feat(app_state): write to url, add txt query

--- app.py
+++ app.py
@@ -12,21 +12,31 @@ st.title("Jina Search")
 app_state = st.experimental_get_query_params()
 app_state = {k: v[0] if isinstance(v, list) else v for k, v in app_state.items()} # fetch the first item in each query string as we don't have multiple values for each query string key in this example
 
-media_option = ["Text", "Image", "Draw"]
+media_option = ["text", "image", "draw"]
 
 st.sidebar.title("Options")
 
-media_default = int(app_state["media"]) if "media" in app_state else 1
-media = st.sidebar.selectbox(label="Media", options=media_option, index=media_default)
+media_default = media_option.index(app_state["media"]) if "media" in app_state else 1
+media = st.sidebar.selectbox(label="Media", options=[i.capitalize() for i in media_option], index=media_default)
+
+app_state["media"] = media.lower()
+st.experimental_set_query_params(**app_state)
 
 endpoint_default = app_state["endpoint"] if "endpoint" in app_state else Defaults.endpoint
 endpoint = st.sidebar.text_input("Endpoint", value=endpoint_default)
+app_state["endpoint"] = endpoint
+st.experimental_set_query_params(**app_state)
 
 top_k_default = int(app_state["top_k"]) if "top_k" in app_state else 10
 top_k = st.sidebar.slider("Top K", min_value=1, max_value=20, value=top_k_default)
+app_state["top_k"] = top_k
+st.experimental_set_query_params(**app_state)
 
 if media == "Text":
+    query_default = app_state["q"] if "q" in app_state else Defaults.text_query
     query = st.text_input("What do you wish to search?", value=Defaults.text_query)
+    app_state["q"] = query
+    st.experimental_set_query_params(**app_state)
 elif media == "Image":
     query = st.file_uploader("File")
 elif media == "Draw":
